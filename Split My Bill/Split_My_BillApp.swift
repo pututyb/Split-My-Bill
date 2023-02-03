2768566335f410af6e0d37e3576d7d40f0bec656
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-02-03 16:33:11 +0700

    Initial Commit

---
+++ Split My Bill/Split_My_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_My_BillApp.swift
+//  Split My Bill
+//
+//  Created by Putut Yusri Bahtiar on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Split_My_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
